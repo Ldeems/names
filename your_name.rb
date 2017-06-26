aa03431fd1ab81f43c8d071f073272794155145e
Ldeems <luke.deems@yahoo.com>
2017-06-26 13:28:51 -0400

    name function

--- your_name.rb
+++ your_name.rb
@@ -1,6 +1,14 @@
-p "Howdy, What is your first name"
-first = gets.chomp.capitalize
-p "What is you last name"
-last = gets.chomp.capitalize
+def first()
+	p "What is your first name?"
+	first = gets.chomp.capitalize
+end
 
+def last() 	
+	p "What is you last name?"
+	last = gets.chomp.capitalize
+	
+end
+
+first = first()
+last = last()
 p "Your name is #{first} #{last}"
